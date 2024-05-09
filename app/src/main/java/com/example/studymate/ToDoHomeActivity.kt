06c34a8a3b3003c1dead756c8655e23c60c04bb3
Nayananjalee <thulasikanayananjalee@gmail.com>
2024-05-09 16:28:14 +0530

    set todo home navigation

--- app/src/main/java/com/example/studymate/ToDoHomeActivity.kt
+++ app/src/main/java/com/example/studymate/ToDoHomeActivity.kt
@@ -1,11 +1,35 @@
 package com.example.studymate
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
 
 class ToDoHomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.to_do_home)
+
+
+        // Find the ImageView with id imageView6 and set an OnClickListener to start MainActivity5
+        val button = findViewById<ImageView>(R.id.imageView6)
+        val intent = Intent(this, InboxActivity::class.java)
+        button.setOnClickListener {
+            startActivity(intent)
+        }
+
+        // Find the ImageView with id imageView10 and set an OnClickListener to start MainActivity4
+        val button2 = findViewById<ImageView>(R.id.imageView7)
+        val intent2 = Intent(this, TodayActivity::class.java)
+        button2.setOnClickListener {
+            startActivity(intent2)
+        }
+        // Find the ImageView with id imageView10 and set an OnClickListener to start MainActivity4
+        val button3 = findViewById<ImageView>(R.id.imageView8)
+        val intent3 = Intent(this, UpcomingActivity::class.java)
+        button3.setOnClickListener {
+            startActivity(intent3)
+        }
     }
 }
