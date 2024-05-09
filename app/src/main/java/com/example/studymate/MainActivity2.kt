46dc50a4fbab1973cb8872daf8fdb9b176e8ad48
Nayananjalee <thulasikanayananjalee@gmail.com>
2024-05-09 13:15:43 +0530

    home page

--- app/src/main/java/com/example/studymate/MainActivity2.kt
+++ app/src/main/java/com/example/studymate/MainActivity2.kt
@@ -1,11 +1,29 @@
 package com.example.studymate
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        // Find the ImageView with id imageView6 and set an OnClickListener to start MainActivity5
+        val button = findViewById<ImageButton>(R.id.imageButton2)
+        val intent = Intent(this, ToDoHomeActivity::class.java)
+        button.setOnClickListener {
+            startActivity(intent)
+        }
+
+        // Find the ImageView with id imageView10 and set an OnClickListener to start MainActivity4
+        val button2 = findViewById<ImageButton>(R.id.imageButton3)
+        val intent2 = Intent(this, ChallengeActivity::class.java)
+        button2.setOnClickListener {
+            startActivity(intent2)
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/studymate/ToDoHomeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.studymate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ToDoHomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.to_do_home)
+    }
+}
