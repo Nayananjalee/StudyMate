96a0a6d497bebb35c2567da4d01393334a110a10
Nayananjalee <thulasikanayananjalee@gmail.com>
2024-05-12 23:51:47 +0530

    complete function

--- app/src/main/java/com/example/studymate/fragment/update/UpdateFragment.kt
+++ app/src/main/java/com/example/studymate/fragment/update/UpdateFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
@@ -41,10 +42,11 @@ class UpdateFragment : Fragment() {
     }
     private fun updateItem(){
         val task = view?.findViewById<EditText>(R.id.update_todo)?.text.toString()
+        val status = view?.findViewById<CheckBox>(R.id.update_status)?.isChecked() ?: false
 
-        val todo = Todo(args.currentTodo.id,task,args.currentTodo.isDone,args.currentTodo.createDate)
+        val todo = Todo(args.currentTodo.id, task, status, args.currentTodo.createDate)
         taskViewModel.updateTodo(todo)
-        Toast.makeText(requireContext(),"Update Successfully!", Toast.LENGTH_LONG).show()
+        Toast.makeText(requireContext(), "Update Successfully!", Toast.LENGTH_LONG).show()
         findNavController().navigate(R.id.action_updateFragment_to_listFragment)
     }
 
