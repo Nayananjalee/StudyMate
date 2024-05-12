2513b4e097054c7b57640ed55ef26652dbb84f1b
Nayananjalee <thulasikanayananjalee@gmail.com>
2024-05-13 02:59:24 +0530

    change ui

--- app/src/main/java/com/example/studymate/fragment/add/AddFragment.kt
+++ app/src/main/java/com/example/studymate/fragment/add/AddFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.DatePicker
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.ImageView
@@ -15,10 +16,12 @@ import androidx.navigation.fragment.findNavController
 import com.example.studymate.R
 import com.example.studymate.viewmodel.TaskViewModel
 import com.example.studymate.model.Todo
+import java.util.Calendar
 
 
 class addFragment : Fragment() {
     private lateinit var taskViewModel: TaskViewModel
+    private lateinit var datePicker: DatePicker
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,6 +31,7 @@ class addFragment : Fragment() {
 
 
         taskViewModel = ViewModelProvider(this).get(TaskViewModel::class.java)
+        datePicker = view.findViewById(R.id.datePicker)
         view.findViewById<ImageView>(R.id.imageView6).setOnClickListener {
             findNavController().navigate(R.id.action_addFragment_to_listFragment)
         }
@@ -40,10 +44,13 @@ class addFragment : Fragment() {
     }
 
     private fun insertDataToDatabase() {
+
         val task = view?.findViewById<EditText>(R.id.todo)
         val text = task?.text.toString()
         val isDone = false
-        val createDate = System.currentTimeMillis()
+        val calendar = Calendar.getInstance()
+        calendar.set(datePicker.year, datePicker.month, datePicker.dayOfMonth)
+        val createDate = calendar.timeInMillis
 
         val todo = Todo(0,text,isDone,createDate)
         taskViewModel.insertTodo(todo)
--- app/src/main/java/com/example/studymate/fragment/update/UpdateFragment.kt
+++ app/src/main/java/com/example/studymate/fragment/update/UpdateFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.CheckBox
+import android.widget.DatePicker
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.ImageView
@@ -19,11 +20,13 @@ import com.example.studymate.R
 import com.example.studymate.TodayActivity
 import com.example.studymate.model.Todo
 import com.example.studymate.viewmodel.TaskViewModel
+import java.util.Calendar
 
 class UpdateFragment : Fragment() {
 
     private val args by navArgs<UpdateFragmentArgs>()
     private lateinit var taskViewModel: TaskViewModel
+    private lateinit var datePicker: DatePicker
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,9 +38,19 @@ class UpdateFragment : Fragment() {
 
 
         taskViewModel = ViewModelProvider(this).get(TaskViewModel::class.java)
+        datePicker = view.findViewById(R.id.datePicker)
         // Get a reference to the EditText view
         val updateTodoEditText = view.findViewById<EditText>(R.id.update_todo)
         updateTodoEditText.setText(args.currentTodo.text)
+        // Set the date in the DatePicker
+        val calendar = Calendar.getInstance()
+        calendar.timeInMillis = args.currentTodo.createDate
+        datePicker.init(
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH),
+            null
+        )
 
         view.findViewById<ImageView>(R.id.imageView6).setOnClickListener {
             findNavController().navigate(R.id.action_updateFragment_to_listFragment)
@@ -59,8 +72,11 @@ class UpdateFragment : Fragment() {
     private fun updateItem(){
         val task = view?.findViewById<EditText>(R.id.update_todo)?.text.toString()
         val status = view?.findViewById<CheckBox>(R.id.update_status)?.isChecked() ?: false
+        val calendar = Calendar.getInstance()
+        calendar.set(datePicker.year, datePicker.month, datePicker.dayOfMonth)
+        val createDate = calendar.timeInMillis
 
-        val todo = Todo(args.currentTodo.id, task, status, args.currentTodo.createDate)
+        val todo = Todo(args.currentTodo.id, task, status, createDate)
         taskViewModel.updateTodo(todo)
         Toast.makeText(requireContext(), "Update Successfully!", Toast.LENGTH_LONG).show()
         findNavController().navigate(R.id.action_updateFragment_to_listFragment)
